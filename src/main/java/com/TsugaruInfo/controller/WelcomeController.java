beacabde37dbcc5b3093c1e054986426c572a02c
shibafu <civaf-th123@hotmail.co.jp>
2018-03-19 11:33:15 +0900

    ウェルカムページ作成

--- src/main/java/com/TsugaruInfo/controller/WelcomeController.java
+++ src/main/java/com/TsugaruInfo/controller/WelcomeController.java
@@ -4,12 +4,22 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class WelcomeController {
 
-	@RequestMapping(value = "/hello", method = GET)
-	public String showMessage(){
-		return "showMessage";
+	@RequestMapping(value = "/", method = GET)
+	public ModelAndView index(){
+		ModelAndView mv = new ModelAndView("index");
+		
+		return mv;
+	}
+	
+	@RequestMapping(value = "/index", method = GET)
+	public ModelAndView index_url(){
+		ModelAndView mv = new ModelAndView("index");
+		
+		return mv;
 	}
 }
--- src/main/webapp/WEB-INF/view/showMessage.jsp
+++ src/main/webapp/WEB-INF/view/showMessage.jsp
@@ -5,7 +5,7 @@
 <html>
 	<head>
 		<meta charset="utf-8">
-		<title>Web水族館プロトプログラム<</title>
+		<title>Web水族館プロトプログラム</title>
 	</head> 
 	<body>
 		<h2>${message}</h2>
